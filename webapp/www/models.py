3b2cf2ff6a9d7567a09b90fa42489e588812af6a
dx19910707 <dx19910707@qq.com>
2018-03-13 17:50:38 +0800

    添加布尔字段，小数字段，文本字段

---
+++ webapp/www/models.py
@@ -0,0 +1,3 @@
+import time,uuid
+
+from orm import Model, StringField
--- webapp/www/orm.py
+++ webapp/www/orm.py
@@ -79,6 +79,21 @@ class IntegerField(Field):
     def __init__(self, name=None, primary_key=False, default=0,):
         super().__init__(name, 'bigint', primary_key, default)
 
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
 class ModelMetaclass(type):
 
     def __new__(cls, name, bases, attrs):
